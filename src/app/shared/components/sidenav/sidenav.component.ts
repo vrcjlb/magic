d843e36f71b2eae55ef6655e7837985cd79a6e35
Victor Rafael Constante Javier Larco Buc <victorlarco@bcp.com.pe>
2023-09-28 17:28:54 -0500

    feat: add feature sidenav

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,11 +1,17 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { Component } from '@angular/core';
+import { NgFor, NgIf, DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatNativeDateModule } from '@angular/material/core';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+
+export interface Menu {
+  label: string;
+  icon: string;
+  link: string;
+  subMenu?: Menu[];
+}
 
 @Component({
   standalone: true,
@@ -13,15 +19,59 @@ import { MatNativeDateModule } from '@angular/material/core';
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
   imports: [
-    MatButtonModule,
-    MatExpansionModule,
+    DatePipe,
+    NgFor,
+    NgIf,
+    RouterLink,
     MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
+    MatButtonModule,
+    MatListModule,
+    MatMenuModule,
   ],
 })
 export class SidenavComponent {
-  @ViewChild(MatAccordion) accordion!: MatAccordion;
+  menu: Menu[] = [
+    {
+      label: 'Dashboard',
+      icon: 'dashboard',
+      link: '/dashboard',
+      subMenu: [
+        {
+          label: 'Dashboard',
+          icon: 'dashboard',
+          link: '/dashboard',
+        },
+        {
+          label: 'Dashboard 2',
+          icon: 'dashboard',
+          link: '/dashboard2',
+        },
+      ],
+    },
+    {
+      label: 'User',
+      icon: 'people',
+      link: '/user',
+    },
+    {
+      label: 'Product',
+      icon: 'shopping_cart',
+      link: '/product',
+    },
+    {
+      label: 'Order',
+      icon: 'shopping_basket',
+      link: '/order',
+    },
+    {
+      label: 'Report',
+      icon: 'report',
+      link: '/report',
+    },
+    {
+      label: 'Setting',
+      icon: 'settings',
+      link: '/setting',
+    },
+  ];
 }
